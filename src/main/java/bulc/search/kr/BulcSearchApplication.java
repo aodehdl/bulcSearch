16157d93effe31f19408366f837a96d6a782bed8
aodehdl <aodehdlwkd2@hanmail.net>
2018-07-07 04:05:28 +0900

    책 검색 시작 프로젝트 생성

---
+++ src/main/java/bulc/search/kr/BulcSearchApplication.java
@@ -0,0 +1,12 @@
+package bulc.search.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulcSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulcSearchApplication.class, args);
+	}
+}
---
+++ src/test/java/bulc/search/kr/BulcSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package bulc.search.kr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BulcSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
